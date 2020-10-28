271e8bda942bc51499891482a531340ca4fa4431
sergiy <vlas.sergiy@gmail.com>
2020-10-29 00:27:34 +0200

    home work 1 - functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('input[name], #formula').forEach(function (input) {
+document.querySelectorAll('input[name], #formula, #condition').forEach(function (input) {
     input.addEventListener('keyup', function () {
         var a1 = document.querySelector('[name="a1"]');
         var b1 = document.querySelector('[name="b1"]');
@@ -12,11 +12,28 @@ document.querySelectorAll('input[name], #formula').forEach(function (input) {
             var calculator = new Function('a1, b1, a2, b2', 'return ' + formula.value + ';');
 
             result.value = calculator(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
+            var exp = new Function('a1, b1, a2, b2', 'return ' + condition.value + ';');
+
+            var x = exp(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
+            if (x === true) {
+                result.style.backgroundColor = '#B6D7A8';
+            }else{
+                result.style.backgroundColor = '#FFFFFF';
+            }
+
+            console.log("Result:" +result.value);
+            console.log("Result:" +x);
+
+
         }
         catch (error) {
             result.value = '#ERROR';
             console.error(error.message);
         }
+
+
 
 
     });
