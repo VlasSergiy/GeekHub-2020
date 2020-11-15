713ccf42813b392461e3bc333b6f71a5753c9de4
sergiy <vlas.sergiy@gmail.com>
2020-11-15 11:10:22 +0200

    homework 3

--- dom/index.js
+++ dom/index.js
@@ -1,79 +1,76 @@
 jQuery('input').on('paste', function (e) {
     e.preventDefault();
 
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
     var text = e.originalEvent.clipboardData.getData('text/plain');
     var input = e.currentTarget;
     var result = [];
     var table = document.querySelector("table tbody");
+    var tableHead = document.querySelector("table thead tr");
 
     text.split("\n").forEach(function(element) {
         result.push(element.split(";"));
     });
 
-    var rowIndex = input.parentNode.parentNode.rowIndex;
-
-    var cellsNumber = addCells(table, result, input);
-    addRows(table, result, rowIndex, cellsNumber);
+    addCells(table, result, input);
+    addRows(table, result, input);
     addValues(table, input, result);
 
+    function addCells(table, arr, input) {
+        var cellIndex = input.parentNode.cellIndex;
+        var rowChildrenCount = input.parentNode.parentNode.children.length - 1;
+        var cellsToAdd = arr[0].length - rowChildrenCount + cellIndex - 1;
+
+        for (i = 0; i < table.children.length; i++) {
+            var row = table.children[i];
+            var rowIndex = row.rowIndex;
+            for (j = 0; j < cellsToAdd; j++) {
+                var cell = document.createElement("td");
+                var inputText = document.createElement('input');
+                inputText.type = 'text';
+                inputText.name = alphabet[(cellIndex == 1 ? cellIndex + 1 : cellIndex) + j] + rowIndex;
+
+                cell.appendChild(inputText);
+                row.appendChild(cell);
+            }
+        }
+
+        for (i = 0; i < cellsToAdd; i++) {
+            var cellTh = document.createElement("th");
+            cellTh.append(document.createTextNode(alphabet[(cellIndex == 1 ? cellIndex + 1 : cellIndex) + i].toUpperCase()));
+            tableHead.append(cellTh);
+        }
+    }
+
+    function addRows(table, arr, input) {
+        var rowIndex = input.parentNode.parentNode.rowIndex;
+        var rowsToAdd = arr.length - table.rows.length + rowIndex - 1;
+        var rowChildrenCount = input.parentNode.parentNode.children.length - 1;
+
+        for (i = 0; i < rowsToAdd; i++) {
+            var row = document.createElement("tr");
+            for (j = 0; j <= rowChildrenCount; j++) {
+
+                var cellValue;
+                var cell = j > 0 ? document.createElement("td") :  document.createElement("th");
+                if (j == 0) {
+                    cellValue = document.createTextNode(table.rows.length + 1);
+                } else {
+                    cellValue = document.createElement('input');
+                    cellValue.type = 'text';
+                    cellValue.name = alphabet[j - 1] + (table.rows.length + 1);
+                }
+
+
+                cell.appendChild(cellValue);
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+    }
 });
 
-function addCells(table, arr, input) {
-    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-    var cellIndex = input.parentNode.cellIndex;
-    var row = input.parentNode.parentNode;
-    var rowChildrenCount = row.children.length - 1;
-    var cellsToAdd = arr[0].length - rowChildrenCount + cellIndex - 1;
-    var tableHead = document.querySelector("table thead tr");
 
-    for (i = 0; i < table.children.length; i++) {
-        var row = table.children[i];
-        var rowIndex = row.rowIndex;
-        for (j = 0; j < cellsToAdd; j++) {
-            var cell = document.createElement("td");
-            var input = document.createElement('input');
-            input.type = 'text';
-            input.name = alphabet[(cellIndex == 1 ? cellIndex + 1 : cellIndex) + j] + rowIndex;
-
-            cell.appendChild(input);
-            row.appendChild(cell);
-        }
-    }
-
-    for (i = 0; i < cellsToAdd; i++) {
-        var cellTh = document.createElement("th");
-        cellTh.append(document.createTextNode(alphabet[(cellIndex == 1 ? cellIndex + 1 : cellIndex) + i].toUpperCase()));
-        tableHead.append(cellTh);
-    }
-
-    return rowChildrenCount + cellsToAdd;
-}
-
-function addRows(table, arr, rowIndex, cellsNumber) {
-    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-    var rowsToAdd = arr.length - table.rows.length + rowIndex - 1;
-
-    for (i = 0; i < rowsToAdd; i++) {
-        var row = document.createElement("tr");
-        for (j = 0; j <= cellsNumber; j++) {
-
-            var cellValue;
-            var cell = j > 0 ? document.createElement("td") :  document.createElement("th");
-            if (j == 0) {
-                cellValue = document.createTextNode(table.rows.length + 1);
-            } else {
-                cellValue = document.createElement('input');
-                cellValue.type = 'text';
-                cellValue.name = alphabet[j - 1] + (table.rows.length + 1);
-            }
-
-
-            cell.appendChild(cellValue);
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
-    }
-}
 
 function addValues(table, input, result) {
     var rowIndex = input.parentNode.parentNode.rowIndex - 1;
