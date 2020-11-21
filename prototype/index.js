18069b89b81e78488be2ee4fba6eb841ae761ea7
sergiy <vlas.sergiy@gmail.com>
2020-11-21 17:24:20 +0200

    home work 4 - prototype

--- prototype/index.js
+++ prototype/index.js
@@ -2,7 +2,11 @@ function Csv() {
 
 }
 
-Csv.prototype.parse = function(string, separator) {
+function CsvArray() {
+
+}
+
+Csv.prototype.parse = function parse(string, separator) {
     const separators = [',', '.', '\t'];
     let allRows = string.split(/\r?\n|\r/);
     let defSeparator = separator;
@@ -20,14 +24,50 @@ Csv.prototype.parse = function(string, separator) {
     return allRows.map(item => item.split(defSeparator));
 };
 
-Csv.prototype.generate = function(array, separator) {
+Csv.prototype.generate = function generate(array, separator) {
     let result = '';
-    array.forEach(function(elem, i) {
+    array.forEach(function(elem) {
         if (!separator) {
-            result += elem + '\r\n';
+            result += elem + '\n';
         } else {
-            result += elem.join(separator) + '\r\n';
+            result += elem.join(separator) + '\n';
         }
     });
     return result;
 };
+
+CsvArray.prototype = Object.create(Array.prototype);
+CsvArray.prototype.parse = function parse(string) {
+    const separators = [',', '.', '\t'];
+    let allRows = string.split(/\r?\n|\r/);
+    let separator;
+
+    separators.forEach(function(elem) {
+        if (allRows[0].split(elem).length - 1 > 0
+            && allRows[0].split(elem).length === allRows[1].split(elem).length
+        ) {
+            separator = elem;
+        }
+    });
+
+    let arr = allRows.map(item => item.split(separator));
+
+    for (let i = 0; i < arr.length; i += 1) {
+        this.push(arr[i]);
+    }
+    return this;
+}
+
+CsvArray.prototype.generate = function generate(separator) {
+    let result = '';
+    this.forEach(function(elem) {
+        if (!separator) {
+            result += elem + '\n';
+        } else {
+            result += elem.join(separator) + '\n';
+        }
+    });
+    return result;
+}
+
+
