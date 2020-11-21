7e93e3fcfe6b7820ccfbeb1d77773c5c9127bb5a
sergiy <vlas.sergiy@gmail.com>
2020-11-21 10:42:46 +0200

    home work 4 - prototype

--- prototype/index.js
+++ prototype/index.js
@@ -1,3 +1,20 @@
 function Csv() {
 
 }
+
+Csv.prototype.parse = function(string, separator) {
+    var separators = ['\,', '\.', '\t'];
+    var allRows = string.split(/\r?\n|\r/);
+
+    if (!separator) {
+        separators.forEach(function(elem) {
+            if ((allRows[0].match(new RegExp(elem)) || []).length > 0 && (allRows[0].match(new RegExp(elem)) || []).length == (allRows[0].match(new RegExp(elem)) || []).length) {
+                separator = elem;
+            }
+        });
+    }
+
+    return allRows.map(item => item.split(separator));
+};
+
+Csv.prototype.generate = function(array, separator) {};
